33e178a93922a6eb6f2171e74d2307d0391f99b3
AchizuePriscilla <baeewele27@gmail.com>
2021-06-07 19:24:48 +0100

    Project initialization and completion

---
+++ lib/customTextField.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class SharedTextField extends StatelessWidget {
+  final bool enabled;
+  final bool obscureText;
+  final InputDecoration inputDecoration;
+
+  const SharedTextField({
+    Key? key,
+    required this.enabled,
+    required this.obscureText,
+    required this.inputDecoration,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      enabled: enabled,
+      obscureText: obscureText,
+      style: TextStyle(
+        color: Color(0xff424242),
+      ),
+      decoration: inputDecoration,
+    );
+  }
+}
+
+const kInputDecoration = InputDecoration(
+    filled: true,
+    fillColor: Colors.white,
+    border: OutlineInputBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(10),
+        ),
+        borderSide: BorderSide.none),
+    hintText: "",
+    hintStyle: TextStyle(color: Color(0xff424242), fontSize: 17));
